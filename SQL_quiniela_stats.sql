78b5b2d39f3d8e411172f97b04fc83eb46a9a86e
Martin Perez <aldomartinperez@hotmail.com>
2023-01-28 17:33:05 -0300

    Updated README.md and finished procedure to unite all tables

--- SQL_quiniela_stats.sql
+++ SQL_quiniela_stats.sql
@@ -50,6 +50,135 @@ READS SQL DATA
 	END$$
 DELIMITER ;
 
+#---------------------------------------------------3----------------------------------------------------------------------------
+
+/*
+The following code if focused on analyzing and clean data for a specific table ('nacional'), to understand what are the main steps required for a stored procedure to automate the cleaning of additional tables -as needed-
+*/
+
+-- Visually check the data
+SELECT * FROM quiniela.nacional
+LIMIT 21;
+
+-- Rename 'date' to 'lottery_date', to avoid using reserved words as column names
+ALTER TABLE quiniela.nacional
+RENAME COLUMN `date` TO lottery_date;
+
+-- Check the total number of occurrences (201.223)
+SELECT COUNT(*)
+FROM quiniela.nacional;
+
+-- Separate position 'Letras' from number positions, into a new table
+CREATE TABLE nacional_letras
+SELECT * 
+FROM quiniela.nacional
+WHERE position = 'Letras';
+
+	-- Visually check new table
+    SELECT *
+    FROM quiniela.nacional_letras
+    LIMIT 50;
+
+	-- Check total number of occurrences (9.563)
+    SELECT COUNT(*)
+    FROM quiniela.nacional_letras;
+
+-- Remove all occurrences with position 'Letras' from original table. Check that rows affected match the total number of occurrences in the newly created table above
+DELETE FROM quiniela.nacional
+WHERE position = 'Letras';
+
+	-- Check total number of occurrences (191.660)
+    SELECT COUNT(*)
+    FROM quiniela.nacional;
+
+/*
+The rest of the code will be only directed to analyze and clean data with a number position, discarding the 'letras' results from the analysis
+*/
+
+-- Check data types of each column
+SHOW FIELDS
+FROM quiniela.nacional;
+
+-- Check for results that do not contain a number
+SELECT DISTINCT(result)
+FROM quiniela.nacional
+ORDER BY result ASC;
+
+-- Remove occurrences where results are not a number (339 rows removed)
+DELETE FROM quiniela.nacional
+WHERE result = '----' OR result = '';
+
+-- Correct format for each of the columns
+ALTER TABLE quiniela.nacional
+MODIFY lottery_date DATE,
+MODIFY quiniela VARCHAR(100),
+MODIFY period VARCHAR(100),
+MODIFY position SMALLINT(2),
+MODIFY result SMALLINT(4) ZEROFILL;
+    
+-- Visually check data
+SELECT *
+FROM quiniela.nacional
+LIMIT 40;
+
+-- Check if the date field has NULL values
+SELECT *
+FROM quiniela.nacional
+WHERE lottery_date IS NULL;
+
+-- Check for columns' values
+SELECT DISTINCT(period)
+FROM quiniela.nacional;
+
+SELECT DISTINCT(position)
+FROM quiniela.nacional;
+
+-- Look at occurrences by period of the day
+SELECT period, COUNT(*) AS number_of_occurrences
+FROM quiniela.nacional
+GROUP BY period
+ORDER BY COUNT(*) DESC;
+
+-- Count the number of days and approximate number of years for which there is lottery data, by period
+SELECT period, COUNT(DISTINCT(lottery_date)) AS number_of_days, (COUNT(DISTINCT(lottery_date)) / 6 / 52) AS number_of_years -- years = total days / 6 (days a week of lotteries) / 52 (weeks in a year)
+FROM quiniela.nacional
+GROUP BY period
+ORDER BY COUNT(DISTINCT(lottery_date)) DESC;
+
+-- Check non-matching dates between the different periods
+
+
+
+
+
+
+
+-- Check that all dates, for each period, have the same number of positions (numbers drawn), by retrieving those whose count is different than 21
+SELECT lottery_date, period, COUNT(*) AS number_of_positions
+FROM quiniela.nacional
+GROUP BY lottery_date, period
+HAVING COUNT(position) != 21;
+
+
+
+
+
+
+#---------------------------------------------------4----------------------------------------------------------------------------
+
+-- Function to create the UNION ALL code needed to unify all available tables within the specified quiniela (i.e., 'nacional', 'buenos_aires', etc)
+DROP FUNCTION IF EXISTS clean_tables;
+DELIMITER $$
+CREATE FUNCTION clean_tables(quiniela_table_name VARCHAR(255)) RETURNS VARCHAR(2000)
+DETERMINISTIC
+NO SQL
+READS SQL DATA
+	BEGIN
+	END$$
+DELIMITER ;
+
+
+#---------------------------------------------------5----------------------------------------------------------------------------
 
 -- Procedure to create 1 unified table with all the period for a specific 'quiniela' (i.e., 'nacional', 'buenos_aires', etc)
 DROP PROCEDURE IF EXISTS process_quiniela;
@@ -67,7 +196,7 @@ CREATE PROCEDURE process_quiniela(IN quiniela_name VARCHAR(255))
 	END$$
 DELIMITER ;
 
+#---------------------------------------------------6----------------------------------------------------------------------------
 
+-- Execute stored procedure for 'Quiniela nacional'
 CALL process_quiniela('nacional');
-
-
